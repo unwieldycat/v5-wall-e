0bda8e53c74019b760adcfe259697dd41a56c4b7
Thurston Yates <thurstonalexyates@gmail.com>
2023-03-04 18:44:59 -0500

    Create battery animation
    Make battery animate on startup

--- include/gui.h
+++ include/gui.h
@@ -1,4 +1,5 @@
 #include "vex.h"
+#include "devices.h"
 #include "resources/indicator.h"
 
 #define MAIN_COLOR lv_color_hex(0xfafa02)
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,9 +1,32 @@
 #include "gui.h"
 
+// =============================== Variables =============================== //
+
+lv_style_t title_style;
+lv_style_t cont_style;
+lv_style_t bar_style;
+lv_style_t bar_style_on;
+
+std::vector<lv_obj_t*> bars;
+
+// ======================== Battery Widget Functions ======================== //
+
+void battery_anim() {
+  double wait_time = 750;
+  for (int i = 0; i < bars.size(); i++) {
+    lv_obj_t * bar = bars.at(i);
+    lv_obj_add_style(bar, &bar_style_on, LV_PART_MAIN);
+
+    vex::wait(wait_time, vex::timeUnits::msec);
+    if (i == 1) wait_time = 100;
+  }
+}
+
+// ============================= Initialization ============================= //
+
 void init_gui() {
   lv_disp_set_rotation(lv_disp_get_default(), LV_DISP_ROT_90);
 
-  static lv_style_t title_style;
   lv_style_set_text_color(&title_style, MAIN_COLOR);
   lv_style_set_text_font(&title_style, &lv_font_montserrat_20);
   lv_style_set_text_align(&title_style, LV_TEXT_ALIGN_CENTER);
@@ -16,8 +39,7 @@ void init_gui() {
   lv_obj_t *img = lv_img_create(lv_scr_act());
   lv_img_set_src(img, &indicator);
   lv_obj_align(img, LV_ALIGN_TOP_LEFT, 4, 60);
-
-  static lv_style_t cont_style;
+  
   lv_style_set_bg_opa(&cont_style, 0);
   lv_style_set_border_opa(&cont_style, 0);
   lv_style_set_pad_hor(&cont_style, 0);
@@ -32,22 +54,26 @@ void init_gui() {
   lv_obj_set_flex_flow(bar_cont, LV_FLEX_FLOW_COLUMN);
   lv_obj_set_flex_align(bar_cont, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
-  static lv_style_t bar_style;
   lv_style_set_radius(&bar_style, 0);
-  lv_style_set_shadow_color(&bar_style, MAIN_COLOR);
-  lv_style_set_shadow_width(&bar_style, LV_DPX(15));
-  lv_style_set_shadow_spread(&bar_style, LV_DPX(5));
   lv_style_set_border_opa(&bar_style, LV_OPA_0);
-  lv_style_set_bg_color(&bar_style, MAIN_COLOR);
+  lv_style_set_bg_color(&bar_style, lv_color_hex(0x262626));
+  
+  lv_style_set_shadow_color(&bar_style_on, MAIN_COLOR);
+  lv_style_set_shadow_width(&bar_style_on, LV_DPX(15));
+  lv_style_set_shadow_spread(&bar_style_on, LV_DPX(5));
+  lv_style_set_bg_color(&bar_style_on, MAIN_COLOR);
 
   for (int i = 0; i < 10; i++) {
     lv_obj_t *bar = lv_obj_create(bar_cont);
     lv_obj_add_style(bar, &bar_style, LV_PART_MAIN);
     lv_obj_set_scrollbar_mode(bar, LV_SCROLLBAR_MODE_OFF);
+    bars.insert(bars.begin(), bar);
 
     if (i == 9)
       lv_obj_set_size(bar, 144, 24);
     else
       lv_obj_set_size(bar, 144, 10);
   }
+
+  battery_anim();
 }
