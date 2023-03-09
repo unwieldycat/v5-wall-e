ef164759e56154be0bb847d0633a6f04f573d2d7
Thurston Yates <thurstonalexyates@gmail.com>
2023-03-08 20:49:08 -0500

    Add drivetrain motors

--- include/devices.h
+++ include/devices.h
@@ -1,5 +1,18 @@
 #include "vex.h"
 
-// Device declarations
+// ========================== Device Declarations ========================== //
+
+// HID & Brain
 extern vex::brain Brain;
+extern vex::controller Controller;
+
+// Drivetrain
+extern vex::motor DriveFL;
+extern vex::motor DriveFR;
+extern vex::motor DriveRL;
+extern vex::motor DriveRR;
+
+// Fake drive
+extern vex::motor TreadL;
+extern vex::motor TreadR;
 
--- src/devices.cpp
+++ src/devices.cpp
@@ -1,4 +1,17 @@
 #include "devices.h"
 
-// Device definitions
+// =========================== Device Definitions =========================== //
+
+// HID & Brain
 vex::brain Brain;
+vex::controller Controller(vex::controllerType::primary);
+
+// Drivetrain
+vex::motor DriveFL(2);
+vex::motor DriveFR(12);
+vex::motor DriveRL(3);
+vex::motor DriveRR(13);
+
+// Fake drive
+vex::motor TreadL(1);
+vex::motor TreadR(11);
